6b2711ec5e7d8c57b62bcd6d851861d8a405d2ed
jpb06 <jpb.06@outlook.com>
2021-04-25 14:37:47 +0200

    Moving swagger to root

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
