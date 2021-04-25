7acd26c49c8e69ab127935714644912d51a6cbe4
jpb06 <jpb.06@outlook.com>
2021-04-25 14:08:09 +0200

    Letting Heroku have a say on the port to listen to

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
