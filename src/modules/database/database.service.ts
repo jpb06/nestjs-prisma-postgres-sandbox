1a8961dda788f331c4aff3a77f02a74eb45a0324
jpb06 <jpb.06@outlook.com>
2021-05-09 20:49:27 +0200

    Module: prisma => database

--- src/modules/database/database.service.ts
+++ src/modules/database/database.service.ts
@@ -2,7 +2,7 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
+export class DatabaseService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
