e52f1503fd6edadfd7197a7c30c825101476bdac
jpb06 <jpb.06@outlook.com>
2021-05-15 14:38:08 +0200

    Cross platform prisma models merging

---
+++ scripts/generate.prisma.schema.ts
@@ -0,0 +1,37 @@
+import { outputFile, readdir, readFile, stat } from 'fs-extra';
+import { EOL } from 'os';
+import * as path from 'path';
+
+const getAllFiles = async (
+  dirPath: string,
+  arrayOfFiles: Array<string> = [],
+  extension?: string,
+) => {
+  const files = await readdir(dirPath);
+  for (const file of files) {
+    const filePath = path.join(dirPath, '/', file);
+    const fileStats = await stat(filePath);
+
+    if (fileStats.isDirectory()) {
+      arrayOfFiles = await getAllFiles(filePath, arrayOfFiles, extension);
+    } else {
+      if (!extension || file.endsWith(extension)) {
+        arrayOfFiles.push(filePath);
+      }
+    }
+  }
+
+  return arrayOfFiles;
+};
+
+(async () => {
+  const basePath = './src/modules';
+  const prismaFiles: Array<string> = [];
+
+  const files = await getAllFiles(basePath, prismaFiles, '.prisma');
+  console.info(`Found ${files.length} prisma models:`, files);
+
+  const data = await Promise.all(files.map((path) => readFile(path)));
+  await outputFile('./prisma/schema.prisma', data.join(EOL));
+  console.info('Prisma schemas merged');
+})();
