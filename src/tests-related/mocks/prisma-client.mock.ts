a4000408fb4aff0afe670954c083796077d25785
jpb06 <jpb.06@outlook.com>
2021-05-19 16:52:31 +0200

    No longer needed

--- src/tests-related/mocks/prisma-client.mock.ts
+++
@@ -1,7 +0,0 @@
-import { mockDeep } from 'jest-mock-extended';
-
-import { PrismaClient as OriginaPrismaClient } from '@prisma/client';
-
-const mockPrisma = mockDeep<OriginaPrismaClient>();
-
-export const MockedPrismaClient = () => mockPrisma;
