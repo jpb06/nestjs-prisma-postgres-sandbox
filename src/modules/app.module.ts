0c7946d1460a91125526836321290be0386f14cc
jpb06 <jpb.06@outlook.com>
2021-05-23 08:58:34 +0200

    Including books module to the app

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { BooksModule } from './books/book/books.module';
 import { UsersModule } from './users/user/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule],
 })
 export class AppModule {}
