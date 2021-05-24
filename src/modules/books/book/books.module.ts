42d59f6d8671cb2d1d63422c05668acb836fae38
jpb06 <jpb.06@outlook.com>
2021-05-24 20:27:06 +0200

    Missing auth module

--- src/modules/books/book/books.module.ts
+++ src/modules/books/book/books.module.ts
@@ -1,11 +1,12 @@
 import { DatabaseModule } from '@database/database.module';
+import { AuthModule } from '@modules/users/auth/auth.module';
 import { Module } from '@nestjs/common';
 
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [BooksController],
   providers: [BooksService],
 })
