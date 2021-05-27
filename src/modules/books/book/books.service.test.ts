69ad3d9a05f89acac55bd87baa03206b81f17438
jpb06 <jpb.06@outlook.com>
2021-05-27 19:52:45 +0200

    Using imported mock data & adding a new test case

--- src/modules/books/book/books.service.test.ts
+++ src/modules/books/book/books.service.test.ts
@@ -6,7 +6,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient } from '@prisma/client';
 import {
   mockedBook,
+  mockedBooks,
   mockedUpdatedBook,
+  newMockedBook,
 } from '@tests/mock-data/books.mock-data';
 
 import { BooksService } from './books.service';
@@ -30,25 +32,14 @@ describe('Books service', () => {
   });
 
   it('should create a book', async () => {
-    const newBook = {
-      idAuthor: 1,
-      idCategory: 1,
-      name: 'Hyperion',
-      publicationDate: 1989,
-    };
-    const persistedBook = {
-      id: 1,
-      createdAt: new Date(),
-      ...newBook,
-    };
-    dbMock.book.create.mockResolvedValueOnce(persistedBook);
+    dbMock.book.create.mockResolvedValueOnce(mockedBook);
 
-    const result = await service.create(newBook);
+    const result = await service.create(newMockedBook);
 
     expect(dbMock.book.create).toHaveBeenCalledWith({
-      data: newBook,
+      data: newMockedBook,
     });
-    expect(result).toStrictEqual(persistedBook);
+    expect(result).toStrictEqual(mockedBook);
   });
 
   it('should throw an error if the book to update does not exist', async () => {
@@ -93,29 +84,11 @@ describe('Books service', () => {
   });
 
   it('should get all books', async () => {
-    const books = [
-      {
-        id: 1,
-        idAuthor: 1,
-        idCategory: 1,
-        name: 'Hyperion',
-        publicationDate: 1989,
-        createdAt: new Date(),
-      },
-      {
-        id: 2,
-        idAuthor: 1,
-        idCategory: 1,
-        name: 'Yola',
-        publicationDate: 2020,
-        createdAt: new Date(),
-      },
-    ];
-    dbMock.book.findMany.mockResolvedValueOnce(books);
+    dbMock.book.findMany.mockResolvedValueOnce(mockedBooks);
 
     const result = await service.getAll();
 
-    expect(result).toStrictEqual(books);
+    expect(result).toStrictEqual(mockedBooks);
   });
 
   it('should get a book by its name', async () => {
@@ -128,6 +101,21 @@ describe('Books service', () => {
     expect(dbMock.book.findFirst).toHaveBeenCalledWith({
       where: {
         name,
+      },
+    });
+    expect(result).toStrictEqual(mockedBook);
+  });
+
+  it('should get a book by its id', async () => {
+    const id = 1;
+
+    dbMock.book.findFirst.mockResolvedValueOnce(mockedBook);
+
+    const result = await service.getById(id);
+
+    expect(dbMock.book.findFirst).toHaveBeenCalledWith({
+      where: {
+        id,
       },
     });
     expect(result).toStrictEqual(mockedBook);
