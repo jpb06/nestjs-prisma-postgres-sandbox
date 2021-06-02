3c6c91a5abc978eb0b093ce7fab17a73343a5180
jpb06 <jpb.06@outlook.com>
2021-06-02 19:17:47 +0200

    Plugging in categories module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthorsModule } from './books/author/authors.module';
 import { BooksModule } from './books/book/books.module';
+import { CategoriesModule } from './books/categories/categories.module';
 import { UsersModule } from './users/user/users.module';
 
 @Module({
-  imports: [UsersModule, BooksModule, AuthorsModule],
+  imports: [UsersModule, BooksModule, AuthorsModule, CategoriesModule],
 })
 export class AppModule {}
