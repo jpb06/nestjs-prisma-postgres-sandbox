40fcd3e42fc3342aec3b9c2b4768d9d4b351d4dc
jpb06 <jpb.06@outlook.com>
2021-06-19 11:52:51 +0200

    Properly documenting bad request message property

--- src/types/dto/bad-request-response.dto.ts
+++ src/types/dto/bad-request-response.dto.ts
@@ -4,8 +4,18 @@ export class BadRequestDto {
   @ApiProperty()
   statusCode: number;
 
-  @ApiProperty()
-  message: [string];
+  @ApiProperty({
+    oneOf: [
+      { type: 'string' },
+      {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      },
+    ],
+  })
+  message: string | [string];
 
   @ApiProperty()
   error: string;
