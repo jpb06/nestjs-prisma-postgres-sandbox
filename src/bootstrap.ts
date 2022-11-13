78666dc780a8e9fdd4e8c0ba4fe1037c1f48474c
jpb06 <jp.bois.06@outlook.fr>
2022-11-13 08:33:21 +0100

    chore: exposing swagger to root

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -22,7 +22,7 @@ export const bootstrap = async (): Promise<INestApplication> => {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   const port = process.env.PORT || 3001;
   await app.listen(port, '0.0.0.0');
