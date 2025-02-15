80b4f194fcc509706373cc42af686188c6f89748
jpb06 <jp.bois.06@outlook.fr>
2025-02-15 14:20:06 +0100

    chore: bumping dependencies (#39)
    * chore: bumping dependencies
    
    * fix: staying on eslint v8
    
    * fix: changing sonar action
    
    * fix: oopsie

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,6 +4,7 @@ import { bootstrap } from './bootstrap';
 
 describe('Main file', () => {
   it('should bootstrap', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
     require('./main');
 
     expect(bootstrap).toHaveBeenCalledTimes(1);
--- src/modules/users/auth/constants/constants.ts
+++ src/modules/users/auth/constants/constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET ?? 'jwt-secret-not-set',
 };
